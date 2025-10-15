bc4869efffaabb906b5c5e2b1145a247d12db9f9
siglee <ghlee@forbiz.co.kr>
2025-10-15 14:33:07 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/kspo/carefit/CareFitApplication.java
@@ -0,0 +1,13 @@
+package com.kspo.carefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kspo/carefit/CareFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kspo.carefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
