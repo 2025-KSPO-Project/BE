493fe8c56668596b9b0a0b9e0e6e20a02356daef
KangWooJu <space1102@naver.com>
2025-11-07 17:31:51 +0900

    refactor : HttpSecurity 객체의 빌드를 묶어서 진행하도록 변경

--- src/main/java/com/kspo/carefit/base/config/security/SecurityConfig.java
+++ src/main/java/com/kspo/carefit/base/config/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.kspo.carefit.base.config.security;
 
+import com.kspo.carefit.base.security.filter.JwtFilter;
 import com.kspo.carefit.base.security.handler.LoginSuccessHandler;
 import com.kspo.carefit.base.security.handler.LogoutSuccessHandlerImpl;
 import com.kspo.carefit.base.security.service.Oauth2UserServiceImpl;
+import com.kspo.carefit.base.security.util.JwtUtil;
+import com.kspo.carefit.damain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -21,35 +25,36 @@ public class SecurityConfig {
     private final Oauth2UserServiceImpl oauth2UserServiceImpl;
     private final LoginSuccessHandler loginSuccessHandler;
     private final LogoutSuccessHandlerImpl logoutSuccessHandler;
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http
-                .csrf(AbstractHttpConfigurer::disable);
-        http
-                .formLogin(AbstractHttpConfigurer::disable);
-        http
-                .httpBasic(AbstractHttpConfigurer::disable);
-        http // CORS 설정
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtFilter(jwtUtil,userService),
+                        UsernamePasswordAuthenticationFilter.class)
+                // CORS 설정
                 .cors((corsCustomizer)->corsCustomizer
-                        .configurationSource(corsConfig.configurationSource()));
-        http // 로그인
+                        .configurationSource(corsConfig.configurationSource()))
+                // 로그인
                 .oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
                                 .userService(oauth2UserServiceImpl))
                         .successHandler(loginSuccessHandler)
-                );
-        http // 로그아웃
+                )// 로그아웃
                 .logout(logout-> logout
                         .logoutUrl("/oauth2/logout")
                         .logoutSuccessHandler(logoutSuccessHandler)
                         .invalidateHttpSession(false)
-                        .clearAuthentication(true));
-        http // 세션 설정
+                        .clearAuthentication(true))
+                // 세션 설정
                 .sessionManagement((session)->session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http // URI 허용 설정
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // URI 허용 설정
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
                         .anyRequest().authenticated());
