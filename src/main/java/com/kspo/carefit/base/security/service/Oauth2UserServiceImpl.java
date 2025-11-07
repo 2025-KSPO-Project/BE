f4b195802df5bda4179e093ff53175842a31369d
KangWooJu <space1102@naver.com>
2025-11-07 17:31:00 +0900

    refactor : loadUser 메소드로 트랜잭션 전파범위 변경

--- src/main/java/com/kspo/carefit/base/security/service/Oauth2UserServiceImpl.java
+++ src/main/java/com/kspo/carefit/base/security/service/Oauth2UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.kspo.carefit.base.security.Oauth2UserImpl;
 import com.kspo.carefit.base.security.oauth2.dto.request.CustomOAuth2UserRequest;
 import com.kspo.carefit.base.security.oauth2.dto.response.NaverResponse;
 import com.kspo.carefit.base.security.oauth2.dto.response.OAuth2Response;
+import com.kspo.carefit.damain.user.entity.User;
 import com.kspo.carefit.damain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -23,6 +25,7 @@ public class Oauth2UserServiceImpl extends DefaultOAuth2UserService {
     private final UserFacade userFacade;
 
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest)
             throws OAuth2AuthenticationException {
 
@@ -38,6 +41,7 @@ public class Oauth2UserServiceImpl extends DefaultOAuth2UserService {
         log.info("로그인 한 유저 - {}",oAuth2Response.getName());
         log.info("네이버 제공 access Token : {}",socialAccessToken.getTokenValue());
 
+        // User 와 토큰 객체를 DB에 저장하기
         userFacade.saveUserWithOauth2Token(socialAccessToken.getTokenValue(),
                 username,
                 oAuth2Response.getEmail(),
