abc144d4c2d474e44ac87ae05e0015122dc481f4
siglee <ghlee@forbiz.co.kr>
2025-11-25 22:55:21 +0900

    feat: 스웨거 설정 추가

--- src/main/java/com/kspo/carefit/base/config/app/GeminiConfig.java
+++ src/main/java/com/kspo/carefit/base/config/app/GeminiConfig.java
@@ -2,10 +2,12 @@ package com.kspo.carefit.base.config.app;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
+@ConditionalOnExpression("!'${spring.gemini.api-key:}'.isEmpty() && '${spring.openai.api-key:}'.isEmpty()")
 public class GeminiConfig {
 
     @Value("${spring.gemini.api-key}")
